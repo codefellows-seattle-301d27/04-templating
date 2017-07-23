7ce7847a349d3461a5914fcc2751779b9b819b6b
Ashwini Rao <ashkrao@gmail.com>
2017-07-22 22:38:18 -0700

    Used the function that was returned by handlebars and  filled-in html template for THIS article

--- starter-code/scripts/article.js
+++ starter-code/scripts/article.js
@@ -15,7 +15,7 @@ Article.prototype.toHtml = function() {
   // Done! TODO: Use handlebars to render your articles.
   //       - Get your template from the DOM.
   //       - Now "compile" your template with Handlebars.
-  var $template = $('#handlebarsTemplate').html();
+  var $template = $('#article-handlebarTemplate').html();
   var compiledTemplate = Handlebars.compile($template);
   // REVIEW: If your template will use properties that aren't on the object yet, add them.
   //   Since your template can't hold any JS logic, we need to execute the logic here.
@@ -24,8 +24,9 @@ Article.prototype.toHtml = function() {
   this.daysAgo = parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000);
   this.publishStatus = this.publishedOn ? `published ${this.daysAgo} days ago` : '(draft)';
 
-  // TODO: Use the function that Handlebars gave you to return your filled-in html template for THIS article.
-
+  // Done! TODO: Use the function that Handlebars gave you to return your filled-in html template for THIS article.
+  var html = compiledTemplate(this);
+  return html;
 };
 
 rawData.sort(function(a,b) {
